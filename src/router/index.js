9449e194209347f774921180339b96fe990f9751
Apple <apple@Apples-MacBook-Air.local>
2025-11-25 11:11:15 +0530

    done storing pinia

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../pages/Home.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
 
 const routes = [
-  { path: '/', name: 'home', component: Home },
+  { path: "/", name: "home", component: Home }
 ];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+export default router;
